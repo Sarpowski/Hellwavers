2e23967e715ed5678df86c4b3001c25f47b1348e
Sarpowski <cansarpsakaoglu@gmail.com>
2024-06-26 15:19:13 +0300

    feat : added run and idle animations

--- Assets/Scripts/CharacterMovementJoystick.cs
+++ Assets/Scripts/CharacterMovementJoystick.cs
@@ -2,17 +2,29 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[RequireComponent(typeof(Rigidbody), typeof(BoxCollider))]
+[RequireComponent(typeof(Rigidbody), typeof(Collider))]
 public class CharacterMovementJoystick : MonoBehaviour
 {
    [SerializeField] private Rigidbody _rigidbody;
    [SerializeField] private FixedJoystick _joystick;
    [SerializeField] private Animator _animator;
    [SerializeField] private float _moveSpeed= 2.0f;
-
+   
    private void FixedUpdate()
    {
       _rigidbody.velocity = new Vector3(_joystick.Horizontal * _moveSpeed, _rigidbody.velocity.y,
          _joystick.Vertical * _moveSpeed);
+
+
+      float moveAmount = _rigidbody.velocity.magnitude;
+      Vector3 targetForwardDirection = _rigidbody.velocity;
+     
+      //get the rotation that corresponds to facing in the direction of the velocity
+      Quaternion targetRotation = Quaternion.LookRotation(targetForwardDirection);
+     
+      //explicity set the rotation of the rigidbody
+      _rigidbody.MoveRotation(targetRotation);
+      _animator.SetFloat("MoveAmount", moveAmount);
+      
    }
 }
