c5b02c518318b09f3b0c86675cc053baec0b28f7
Sarpowski <cansarpsakaoglu@gmail.com>
2024-07-02 17:50:58 +0300

    fix : wave manager

--- Assets/Scripts/Managers/EnemyManager.cs
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -2,50 +2,90 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using Random = UnityEngine.Random;
 
 public class EnemyManager : MonoBehaviour
 {
     public Transform[] m_spawnPoints;
     public GameObject m_EnemyPrefab;
-    public float spawnInterval = 25f;
-    public int maxEnemies = 25;
+    
+    public float spawnInterval = 1f; // Time between enemy spawns within a wave
+    public int enemiesPerWave = 5;
     public int enemyCount = 0;
+   
+    public float difficultyMultiplier = 1.1f;
+    public int currentWave = 0;
+    public float timeBetweenWaves = 5f;
+    private bool waveInProgress = false;
     void Start()
     {
-      StartCoroutine(SpawnEnemies());
+        StartCoroutine(ManageWaves());
     }
 
-    
-    private IEnumerator SpawnEnemies()
+    private IEnumerator ManageWaves()
     {
-        while (enemyCount < maxEnemies)
+        while (true)
+        {
+            if (!waveInProgress)
+            {
+                waveInProgress = true;
+                yield return StartCoroutine(SpawnWave());
+                yield return new WaitForSeconds(timeBetweenWaves); // Wait between waves
+                waveInProgress = false;
+                
+            }
+        }
+        yield return null;
+    }
+
+    private IEnumerator SpawnWave()
+    {
+        currentWave++;
+        Debug.Log("current wave " + currentWave);
+        int enemiesToSpawn = Mathf.RoundToInt(enemiesPerWave * Mathf.Pow(difficultyMultiplier, currentWave - 1));
+
+        for (int i = 0; i < enemiesToSpawn; i++)
         {
             SpawnNewEnemy();
-            yield return new WaitForSeconds(spawnInterval); // Belirtilen süre kadar bekle
+            yield return new WaitForSeconds(spawnInterval); 
+        }
+    }
+
+    private IEnumerator SpawnEnemies()
+    {
+        while (enemyCount < enemiesPerWave )
+        {
+            SpawnNewEnemy();
+            yield return new WaitForSeconds(spawnInterval); 
         }
     }
 
 
-    // private void SpawnNewEnemy()
-    // {
-    //     int randomNumber = Mathf.RoundToInt(Random.Range(0f, m_spawnPoints.Length - 1));
-    //     
-    //     Instantiate(m_EnemyPrefab, m_spawnPoints[randomNumber].transform.position, Quaternion.identity);
-    // }
     private void SpawnNewEnemy()
     {
         if (m_EnemyPrefab != null && m_spawnPoints != null && m_spawnPoints.Length > 0)
         {
-            int randomNumber = Mathf.RoundToInt(Random.Range(0f, m_spawnPoints.Length - 1));
-            Transform spawnPoint = m_spawnPoints[randomNumber];
+            int randomIndex = Random.Range(0, m_spawnPoints.Length);
+            Transform spawnPoint = m_spawnPoints[randomIndex];
 
             if (spawnPoint != null)
             {
-                Instantiate(m_EnemyPrefab, spawnPoint.position, Quaternion.identity);
+                GameObject newEnemy = Instantiate(m_EnemyPrefab, spawnPoint.position, spawnPoint.rotation);
+                EnemyAi enemyAI = newEnemy.GetComponent<EnemyAi>();
+
+                if (enemyAI != null)
+                {
+                    enemyAI._health = Mathf.RoundToInt(enemyAI._health * Mathf.Pow(difficultyMultiplier, currentWave - 1));
+                    NavMeshAgent agent = newEnemy.GetComponent<NavMeshAgent>();
+                    if (agent != null)
+                    {
+                        agent.speed *= Mathf.Pow(difficultyMultiplier, currentWave - 1);
+                    }
+                }
+
                 enemyCount++; // Increment the enemy counter
             }
         }
     }
-    
 }
